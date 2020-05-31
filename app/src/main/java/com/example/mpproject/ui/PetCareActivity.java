bf60794cbdce34d62dd3369e3f5d1d850e0bd2c9
ktu0113 <ktu0113@naver.com>
2020-05-31 11:13:29 +0900

    Add app_bar_petcare, content_petcare Modify activity_petcare, PetcareActivity to add navigation bar.

--- app/src/main/java/com/example/mpproject/ui/PetCareActivity.java
+++ app/src/main/java/com/example/mpproject/ui/PetCareActivity.java
@@ -1,20 +1,39 @@
 package com.example.mpproject.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.mpproject.Fragments.PetcareListFragment;
 import com.example.mpproject.Fragments.ShopinfoFragment;
 import com.example.mpproject.R;
+import com.example.mpproject.listeners.NavigationViewItemListener;
+import com.google.android.material.navigation.NavigationView;
 
 public class PetCareActivity extends AppCompatActivity implements PetcareListFragment.onShopSelectedListener{
     PetcareListFragment mPetcareListFragment = new PetcareListFragment();
+    NavigationView mNv;
+    DrawerLayout mDl;
+    Toolbar mTb;
 
     public void initView(){
+        mTb = findViewById(R.id.toolbar);
+        setSupportActionBar(mTb);
+        //SetToolbar
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //햄버거 메뉴 활성화
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_hamburger_menu);
+        //햄버거 메뉴 아이콘 설정
+
+        mNv = findViewById(R.id.nav_view);
+        mDl = findViewById(R.id.drawer_layout);
 
     }
 
@@ -22,6 +41,9 @@ public class PetCareActivity extends AppCompatActivity implements PetcareListFra
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_petcare);
+
+        initView();
+        mNv.setNavigationItemSelectedListener(new NavigationViewItemListener(this));
 
         if(findViewById(R.id.petcare_fragment_container)!=null){
             mPetcareListFragment = new PetcareListFragment();
@@ -52,5 +74,16 @@ public class PetCareActivity extends AppCompatActivity implements PetcareListFra
             transaction.replace(R.id.petcare_fragment_container, newFragment);
             newFragment.updateShopinfoView(position);
         }
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        switch (id){
+            case android.R.id.home: // 좌측의 햄버거 메뉴 버튼을 눌렀을 때
+                mDl.openDrawer(mNv); //Drawer의 NavigationView가 튀어나오도록 해준다.
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
